abca4fa2a33211c9727babec728b41bf4df2f5a6
namrur <35817877+namrur@users.noreply.github.com>
2023-05-19 08:45:11 +0300

    Create massage.php

---
+++ massage.php
@@ -0,0 +1,10 @@
+
+{
+“app”: “Name of the incoming message app”,
+“sender”: “A person’s name or number who is sending you the message”,
+“message”: “Incoming message”
+}
+
+{
+“reply”: “Hi, we have received your request, thanks for contacting”
+}
